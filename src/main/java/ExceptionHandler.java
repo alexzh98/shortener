e94c6b18914dd3495755fc6e7a038665445dafa4
alex <zhivoderov.aleksej@gmail.com>
2021-09-08 18:04:22 +0300

    одно из заданий курса JR

---
+++ src/main/java/ExceptionHandler.java
@@ -0,0 +1,10 @@
+//Обработчик исключительных ситуаций
+
+public class ExceptionHandler {
+
+    public static void log(Exception e) {
+        System.out.println(e.toString());
+    }
+}
+
+
---
+++ src/main/java/Helper.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Helper {
+
+    //генерировать случайную строку
+    public static String generateRandomString() {
+
+        SecureRandom secureRandom = new SecureRandom();
+        return new BigInteger(100, secureRandom).toString(32);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
